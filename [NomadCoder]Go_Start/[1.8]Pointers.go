1f20055648f67933f9511b8dfb4868e87f5afd5b
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-06-27 22:09:14 +0900

    Update [1.8]Pointers.go

--- [NomadCoder]Go_Start/[1.8]Pointers.go
+++ [NomadCoder]Go_Start/[1.8]Pointers.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-  // 값 복사
+	// 값 복사
 	// a := 2
 	// b := a
 	// a = 10
 	// fmt.Println(a, b)
-  
-  // 주소 확인
+
+	// 주소 확인
 	// a := 2
 	// b := 5
 	// fmt.Println(&a, &b)
 
-  // Pointer 변수 생성
+	// Pointer 변수 생성
 	// a := 2
 	// b := &a
 	// fmt.Println(&a, b)
@@ -24,13 +24,13 @@ func main() {
 	// a = 5
 	// fmt.Println(*b)
 
-  // Pointer를 통한 접근
+	// Pointer를 통한 접근
 	// a := 2
 	// b := &a
 	// *b = 20
 	// fmt.Println(a)
 
-  // 정리
+	// 정리
 	a := 2
 	b := &a
 	*b = 202020
