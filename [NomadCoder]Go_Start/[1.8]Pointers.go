bc6fea57df7da71621d8eb09b4040b61e7e8f1a1
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-06-27 22:09:54 +0900

    Update [1.8]Pointers.go

--- [NomadCoder]Go_Start/[1.8]Pointers.go
+++ [NomadCoder]Go_Start/[1.8]Pointers.go
@@ -3,36 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	// 값 복사
-	// a := 2
-	// b := a
-	// a = 10
-	// fmt.Println(a, b)
+// 값 복사
+// a := 2
+// b := a
+// a = 10
+// fmt.Println(a, b)
 
-	// 주소 확인
-	// a := 2
-	// b := 5
-	// fmt.Println(&a, &b)
+// 주소 확인
+// a := 2
+// b := 5
+// fmt.Println(&a, &b)
 
-	// Pointer 변수 생성
-	// a := 2
-	// b := &a
-	// fmt.Println(&a, b)
-	// fmt.Println(a, b)
-	// fmt.Println(b)
-	// fmt.Println(*b)
-	// a = 5
-	// fmt.Println(*b)
+// Pointer 변수 생성
+// a := 2
+// b := &a
+// fmt.Println(&a, b)
+// fmt.Println(a, b)
+// fmt.Println(b)
+// fmt.Println(*b)
+// a = 5
+// fmt.Println(*b)
 
-	// Pointer를 통한 접근
-	// a := 2
-	// b := &a
-	// *b = 20
-	// fmt.Println(a)
+// Pointer를 통한 접근
+// a := 2
+// b := &a
+// *b = 20
+// fmt.Println(a)
 
-	// 정리
-	a := 2
-	b := &a
-	*b = 202020
-	fmt.Println(a)
+// 정리
+a := 2
+b := &a
+*b = 202020
+fmt.Println(a)
 }
