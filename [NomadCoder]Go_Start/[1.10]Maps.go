ce9391161a090a7852d6839ce804a265a50c28b6
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-06-29 23:43:45 +0900

    Create [1.10]Maps.go

---
+++ [NomadCoder]Go_Start/[1.10]Maps.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nico := map[string]string{"name": "nico", "age": "12"}
+	// fmt.Println(nico)
+
+	// for key, value := range nico {
+	// 	fmt.Println(key, value)
+	// }
+
+	// for _, value := range nico {
+	// 	fmt.Println(value)
+	// }
+
+	for key, _ := range nico {
+		fmt.Println(key)
+	}
+}
