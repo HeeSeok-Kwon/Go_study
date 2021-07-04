621e43ab410c4e49913d061da727e43f4dccb8f5
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-04 22:14:48 +0900

    Create main.go

---
+++ [NomadCoder]Go_Start/[2.2]Methods_part_Two/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"accounts"
+	"fmt"
+)
+
+func main() {
+	// use public struct
+	// account := banking.Account{Owner: "nicolas", Balance: 1000}
+	// account.Owner = "pepe"
+
+	// use NewAccount function
+	account := accounts.NewAccount("nico")
+	// account.owner = "pepe" // 바꿀 수 없다.
+	// account.balance = 1000 // 바꿀 수 없다.
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		// log.Fatalln(err)
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
+}
