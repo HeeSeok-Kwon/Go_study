aac1c2a6cfcd809abb52582c3a7dfd571459c0f4
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-29 23:30:27 +0900

    Create main.go

---
+++ [NomadCoder]Go_Start/[4.7]More_Channels_With_GoRoutines/main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	ccsv "github.com/tsak/concurrent-csv-writer"
+)
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+func main() {
+	var jobs []extractedJob
+	c := make(chan []extractedJob)
+	totalPages := getPages()
+	// fmt.Println(totalPages)
+
+	for i := 0; i < totalPages; i++ {
+		// extractedJobs := getPage(i)
+		// jobs = append(jobs, extractedJobs...) // array merge
+		go getPage(i, c)
+	}
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-c
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	writeJobs(jobs)
+	// writeJobswithGoRoutines(jobs)
+	fmt.Println("Done, extracted", len(jobs))
+}
+
+func getPage(page int, mainC chan<- []extractedJob) {
+	var jobs []extractedJob
+	c := make(chan extractedJob)
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close() // 이 함수 끝났을 때 닫아줘야 함
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".tapItem")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		// job := extractJob(card, c)
+		go extractJob(card, c)
+	})
+
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+
+	// return jobs
+	mainC <- jobs
+}
+
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
+	id, _ := card.Attr("data-jk")
+	// id, _ := card.Attr("href")
+	// fmt.Println(id)
+	title := cleanString(card.Find(".jobTitle>span").Text())
+	// fmt.Println(title)
+	location := cleanString(card.Find(".companyLocation").Text())
+	// fmt.Println(location)
+	salary := cleanString(card.Find(".salary-snippet").Text())
+	summary := cleanString(card.Find(".job-snippet").Text())
+	// fmt.Println(id, title, location, salary, summary)
+	// return extractedJob{
+	c <- extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close() // 이 함수 끝났을 때 닫아줘야 함
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	// For next lecture -- Each()
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		// fmt.Println(s.Html())
+		// fmt.Println(s.Find("a").Length())
+		pages = s.Find("a").Length()
+	})
+	// fmt.Println(doc)
+	return pages
+}
+
+// write xcel
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+}
+
+func writeJobswithGoRoutines(jobs []extractedJob) {
+	csv, err := ccsv.NewCsvWriter("sample.csv")
+	checkErr(err)
+
+	defer csv.Close()
+
+	csv.Write([]string{"Link", "Title", "Location", "Salary", "Summary"})
+	done := make(chan bool)
+	for _, job := range jobs {
+		go func(job extractedJob) {
+			csv.Write([]string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary})
+			done <- true
+		}(job)
+	}
+	for i := 0; i < len(jobs); i++ {
+		<-done
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
