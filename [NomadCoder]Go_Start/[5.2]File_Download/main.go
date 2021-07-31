9e0a7c27fe160defa8a1bd2cdb32af0c596e1391
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-31 22:13:22 +0900

    Update main.go

--- [NomadCoder]Go_Start/[5.2]File_Download/main.go
+++ [NomadCoder]Go_Start/[5.2]File_Download/main.go
@@ -32,9 +32,3 @@ func main() {
 // go get github.com/labstack/echo/v4 --> import error
 // 강의에서는 error 때문에 v4 제거함
 // 다시 go get github.com/labstack/echo
-
-// file encoding error --> 한글이 깨짐
-// 해결방안
-// (1) 다운받은 csv file 우클릭 --> 메모장으로 열기
-// (2) 엑셀 빈 파일을 열고 --> 데이터 --> 새쿼리 --> 파일에서 --> csv에서 --> jobs.csv 선택 --> 가져오기 --> 파일원본 드롭다운 버튼 클릭 후 65001:유니코드(UTF-8) 선택 --> 로드 클릭
-// --> 디자인 탭 --> 머리글 행 체크 해제 --> 첫 번째 행 삭제 --> 표스타일 : 없음 선택 --> 저장
